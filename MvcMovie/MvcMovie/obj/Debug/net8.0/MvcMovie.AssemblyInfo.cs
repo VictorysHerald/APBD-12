408c89cfe38b2253a7e557688ff7b763e2ee1492
Maciej Cynkar <s27003@pjwstk.edu.pl>
2024-06-15 18:50:36 +0200

    Done

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89eea3689b7992241ad78f179c520be37353a842")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed98ecdeedb6e8a2e6bf8e06f4a8d108786e8cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
